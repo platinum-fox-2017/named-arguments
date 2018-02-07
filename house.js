2e70ea92fc4b034e25a3117ba985171236dc8eb5
Ervan Adetya <vaanz.adetya@gmail.com>
2018-02-07 14:15:30 +0700

    First Commit

--- house.js
+++ house.js
@@ -1,19 +1,19 @@
 'use strict'
 
 class House {
-
-  constructor(address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, short_sale, has_tenants) {
-    this.address = address
-    this.square_feet = square_feet
-    this.num_bedrooms = num_bedrooms || 3
-    this.num_baths = num_baths || 2
-    this.cost = cost || 320000
-    this.down_payment = down_payment || 0.20
-    this.sold = sold || false
-    this.short_sale = short_sale
-    this.has_tenants = has_tenants || false
+  
+  constructor(obj) {
+    this.address = coolHouse.address
+    this.square_feet = coolHouse.square_feet
+    this.num_bedrooms = coolHouse.num_bedrooms || 3
+    this.num_baths = coolHouse.num_baths || 2
+    this.cost = coolHouse.cost || 320000
+    this.down_payment = coolHouse.down_payment || 0.20
+    this.sold = coolHouse.sold || false
+    this.short_sale = coolHouse.short_sale
+    this.has_tenants = coolHouse.has_tenants || false
   }
-
+  
   obscure_address() {
     this.address.replace(/.{10}$/g, '****')
   }
@@ -33,6 +33,17 @@ class House {
   }
 }
 
-const cool = new House('address', 100, 2, 2, 12345, 12345, true, true)
+let coolHouse={
+  address:'address',
+  square_feet:100,
+  num_bedrooms : 2,
+  num_baths : 2,
+  cost : 12345,
+  down_payment : 12345,
+  sold : true,
+  short_sale : true,
+  has_tenants: true,
+}
 
+const cool = new House(coolHouse);
 console.log(cool.to_s())

50c543635164795fb6d85d2f3b4e7fb5d6907a32
WikaSilo <wikanyaa@gmail.com>
2018-02-07 17:20:30 +0700

    modified syntax with obj

--- house.js
+++ house.js
@@ -1,21 +1,20 @@
 'use strict'
 
 class House {
-
-  constructor(address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, short_sale, has_tenants) {
-    this.address = address
-    this.square_feet = square_feet
-    this.num_bedrooms = num_bedrooms || 3
-    this.num_baths = num_baths || 2
-    this.cost = cost || 320000
-    this.down_payment = down_payment || 0.20
-    this.sold = sold || false
-    this.short_sale = short_sale
-    this.has_tenants = has_tenants || false
+  constructor(obj) {
+    this.address = obj.address
+    this.square_feet = obj.square_feet
+    this.num_bedrooms = obj.num_bedrooms || 3
+    this.num_baths = obj.num_baths || 2
+    this.cost = obj.cost || 320000
+    this.down_payment = obj.down_payment || 0.20
+    this.sold = obj.sold || false
+    this.short_sale = obj.short_sale
+    this.has_tenants = obj.has_tenants || false
   }
 
   obscure_address() {
-    this.address.replace(/.{10}$/g, '****')
+    return this.address.replace(/.{10}$/g, '****')
   }
 
   buy(money, good_credit) {
@@ -33,6 +32,17 @@ class House {
   }
 }
 
-const cool = new House('address', 100, 2, 2, 12345, 12345, true, true)
+var obj = {}
+obj.address = 'address'
+obj.square_feet = 100
+obj.num_bedrooms = 2
+obj.num_baths = 2
+obj.cost = 12345
+obj.down_payment = 12345
+obj.sold = true
+obj.has_tenants = true
+
+
+const cool = new House(obj)
 
 console.log(cool.to_s())

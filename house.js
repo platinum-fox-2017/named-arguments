ebb5aea7975cc1f42c38cb80a00951bf19f22aa2
Reynaldi Ananda Pane <reynaldipane@gmail.com>
2018-02-07 14:36:06 +0700

    Release 0:Use named arguments

--- house.js
+++ house.js
@@ -2,16 +2,16 @@
 
 class House {
 
-  constructor(address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, short_sale, has_tenants) {
-    this.address = address
-    this.square_feet = square_feet
-    this.num_bedrooms = num_bedrooms || 3
-    this.num_baths = num_baths || 2
-    this.cost = cost || 320000
-    this.down_payment = down_payment || 0.20
-    this.sold = sold || false
-    this.short_sale = short_sale
-    this.has_tenants = has_tenants || false
+  constructor(house) {
+    this.address = house.address
+    this.square_feet = house.square_feet
+    this.num_bedrooms = house.num_bedrooms || 3
+    this.num_baths = house.num_baths || 2
+    this.cost = house.cost || 320000
+    this.down_payment = house.down_payment || 0.20
+    this.sold = house.sold || false
+    this.short_sale = house.short_sale
+    this.has_tenants = house.has_tenants || false
   }
 
   obscure_address() {
@@ -33,6 +33,17 @@ class House {
   }
 }
 
-const cool = new House('address', 100, 2, 2, 12345, 12345, true, true)
+var objHouse = {
+  address : 'address',
+  square_feet: 100,
+  num_bedrooms: 2,
+  num_baths: 2,
+  cost: 12345,
+  down_payment: 12345,
+  sold: true,
+  has_tenants: true
+}
+
+const cool = new House(objHouse);
 
 console.log(cool.to_s())

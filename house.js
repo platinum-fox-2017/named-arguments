76b924fee5c0f2451cb2558287c157203a4311fe
root <irsyadpahlapi28@gmail.com>
2018-02-07 02:40:16 -0500

    upload file house name arguments

--- house.js
+++ house.js
@@ -2,20 +2,20 @@
 
 class House {
 
-  constructor(address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, short_sale, has_tenants) {
-    this.address = address
-    this.square_feet = square_feet
-    this.num_bedrooms = num_bedrooms || 3
-    this.num_baths = num_baths || 2
-    this.cost = cost || 320000
-    this.down_payment = down_payment || 0.20
-    this.sold = sold || false
-    this.short_sale = short_sale
-    this.has_tenants = has_tenants || false
+  constructor(cool) {
+    this.address = cool.address
+    this.square_feet = cool.square_feet
+    this.num_bedrooms = cool.num_bedrooms || 3
+    this.num_baths = cool.num_baths || 2
+    this.cost = cool.cost || 320000
+    this.down_payment = cool.down_payment || 0.20
+    this.sold = cool.sold || false
+    this.short_sale = cool.short_sale
+    this.has_tenants = cool.has_tenants || false
   }
 
   obscure_address() {
-    this.address.replace(/.{10}$/g, '****')
+    return this.address.replace(/.{10}$/g, '****')
   }
 
   buy(money, good_credit) {
@@ -32,7 +32,17 @@ class House {
     return `${this.obscure_address()} : ${this.square_feet} sq. ft., ${this.num_bedrooms} bed, ${this.num_baths} bath. ${this.cost}`
   }
 }
-
-const cool = new House('address', 100, 2, 2, 12345, 12345, true, true)
-
+var obj={
+  address:'address',
+  square_feet:100,
+  num_bedrooms:2,
+  num_baths:2,
+  cost:12345,
+  down_payment:12345,
+  sold:true,
+  short_sale:true,
+  has_tenants:true
+}
+const cool = new House(obj)
+// console.log(cool)
 console.log(cool.to_s())

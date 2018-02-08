6d935db5bc97ed1b407379b04bf6706eedf5b9ff
ekifakhrureza <ekifakhrureza@gmail.com>
2018-02-08 11:06:42 +0700

    NAMED ARGUMENTS

--- car.js
+++ car.js
@@ -3,7 +3,7 @@
 class Car {
 
   start_car(options) {
-    if (options && options['headlights']) {
+    if (options && options.hasOwnProperty('headlights')) {
       this.headlights = options['headlights']
     } else {
       this.headlights = true
@@ -18,7 +18,6 @@ class Car {
 }
 
 const herbie = new Car()
-
 // When we start the car, the headlights should be on by default
 herbie.start_car()
 

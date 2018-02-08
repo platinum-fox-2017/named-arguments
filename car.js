9d747b9a9881bc1654d4843aac3a2b050cbc7a32
WikaSilo <wikanyaa@gmail.com>
2018-02-08 11:07:05 +0700

    modified, add hasOwnProperty

--- car.js
+++ car.js
@@ -3,7 +3,7 @@
 class Car {
 
   start_car(options) {
-    if (options && options['headlights']) {
+    if (options && options.hasOwnProperty(['headlights'])) {
       this.headlights = options['headlights']
     } else {
       this.headlights = true
@@ -21,7 +21,6 @@ const herbie = new Car()
 
 // When we start the car, the headlights should be on by default
 herbie.start_car()
-
 console.log(herbie.headlights) // true
 
 // But what happens when we want to start without headlights?
